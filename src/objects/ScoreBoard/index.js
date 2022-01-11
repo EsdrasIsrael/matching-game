aa65302d306b995d9df3fc5675c504c05ac33070
Esdras Israel <esdras.santos@dataprev.gov.br>
2022-01-11 16:55:52 -0300

    Create HTML of the PlayerScore component

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -1,11 +1,14 @@
 import PlayerName from "../../components/PlayerName/index.js"
+import PlayerScore from "../../components/PlayerScore/index.js";
 import VsPlayer from "../../components/VsPlayer/index.js";
 import "./style.css"
 function ScoreBoard(){
     return /*html*/`
         <header class="score-board">
             ${PlayerName("Player1")}
+            ${PlayerScore(3)}
             ${VsPlayer()}
+            ${PlayerScore(1)}
             ${PlayerName("Player2")}
         </header>
     `;
